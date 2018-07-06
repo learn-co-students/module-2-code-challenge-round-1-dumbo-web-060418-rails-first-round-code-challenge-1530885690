46e36fcc284507bf958949604e8093e205abf6e8
rscheiwe <rscheiwe@gmail.com>
2018-07-06 10:47:44 -0400

    YAH

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,14 @@
 class Heroine < ApplicationRecord
+
+  belongs_to :power
+
+  validates :super_name, uniqueness: true
+
+  def self.search(search)
+    if search
+      where('name LIKE ?', "%#{search}%")
+    else
+      all
+    end
+  end
 end
--- app/models/power.rb
+++ app/models/power.rb
@@ -1,2 +1,11 @@
 class Power < ApplicationRecord
+  has_many :heroines
+
+  def self.search(search)
+  	if search
+  		where('name LIKE ?', "%#{search}%")
+  	else
+  		all
+  	end
+  end
 end
